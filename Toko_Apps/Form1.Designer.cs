8b9fee3a86e0cbc2503533bab9a98a2d41df413c
Obenk Ploes <dataprojectobenk@gmail.com>
2025-01-23 08:45:42 +0700

    test

---
+++ Toko_Apps/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Toko_Apps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kode_buku = new System.Windows.Forms.TextBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // kode_buku
+            // 
+            this.kode_buku.Location = new System.Drawing.Point(171, 73);
+            this.kode_buku.Name = "kode_buku";
+            this.kode_buku.Size = new System.Drawing.Size(248, 20);
+            this.kode_buku.TabIndex = 0;
+            this.kode_buku.Enter += new System.EventHandler(this.kode_buku_Enter);
+            this.kode_buku.KeyUp += new System.Windows.Forms.KeyEventHandler(this.kode_buku_KeyUp);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 536);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.kode_buku);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox kode_buku;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Toko_Apps/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace Toko_Apps
+{
+    public partial class Form1 : Form
+    {
+        String config = "Server=localhost;User=root;port=3306;Database=toko_app";
+        MySqlConnection conn = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private AutoCompleteStringCollection getBookData()
+        {
+            conn = new MySqlConnection(connectionString: config);
+
+            AutoCompleteStringCollection book = new AutoCompleteStringCollection();
+            try
+            {
+                conn.Open();
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from books ";
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        book.Add(reader.GetString(1)); ;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return book;
+        }
+
+        private void kode_buku_Enter(object sender, EventArgs e)
+        {
+            kode_buku.AutoCompleteMode = AutoCompleteMode.Suggest;
+            kode_buku.AutoCompleteCustomSource = getBookData();
+            kode_buku.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+
+        private void kode_buku_KeyUp(object sender, KeyEventArgs e)
+        {
+
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString("TOKO Bheka", new Font("Calibri", 12, FontStyle.Bold),Brushes.Black,new Point(10,10));
+            e.Graphics.DrawString("TOKO Bheka", new Font("Calibri", 12, FontStyle.Bold), Brushes.Black, new Point(10, 22));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            printDocument1.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("a4", 300, 600);
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.Show();
+        }
+    }
+}
